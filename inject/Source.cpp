1a2b4065d1a722ac18ce91507ca2cf70d0215058
Keiran Young <kcyoung1997@hotmail.co.uk>
2017-09-03 21:29:55 +0100

    Remove redundant code.

--- inject/Source.cpp
+++ inject/Source.cpp
@@ -3,22 +3,6 @@
 #include <stdarg.h>
 #include <thread>
 
-BOOLEAN WINAPI DllMain(IN HINSTANCE, IN DWORD, IN LPVOID);
-
-typedef void(__cdecl* MsgFn)(const char* msg);
-
-MODULEINFO GetModuleInfo(char *szModule)
-{
-	MODULEINFO modinfo = { nullptr };
-	HMODULE hModule = GetModuleHandle(szModule);
-	if (hModule == nullptr)
-		return modinfo;
-	GetModuleInformation(GetCurrentProcess(), hModule, &modinfo, sizeof(MODULEINFO));
-	return modinfo;
-}
-
-
-
 BOOLEAN WINAPI DllMain(IN HINSTANCE hDllHandle,
 	IN DWORD     nReason,
 	IN LPVOID    Reserved)
